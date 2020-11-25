326c048b3f2ab3811f2ddcb34c0bb67507a00378
Roberto Valenzuela <robval.1721@gmail.com>
2020-11-25 10:51:35 -0300

    feat(global): added global index and app server

---
+++ src/app/server.ts
@@ -0,0 +1,32 @@
+//------------------------------------------LIBRERIAS
+import express, { Express } from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+//------------------------------------------MODULOS
+import logModule from "./modules/log.module";
+import mongooseModule from "./modules/mongoose.module";
+import components from "./components";
+
+async function main() {
+  const server: Express = express();
+  const port: number = parseInt(process.env['PORT'] || '3000');
+
+  server.use(express.json());
+  server.use(morgan('dev'));
+  server.use(cors({ origin: 'http://localhost:16001' }));
+  server.use('/api', ...components);
+
+  try {
+    await mongooseModule.connect();
+    logModule.success('Database connection successful');
+
+    server.listen(port, () => {
+      logModule.success(`Server listening on port ${port}`);
+    });
+  }
+  catch (error) {
+    logModule.error(`Failed database connection`);
+  }
+}
+export default { main };
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * Ossas API
+ * 
+ * @description API REST developed with Node.js, Typescript and Express.
+ * @author Valenzuela, Moore and Wastavino
+ * @version 0.1
+ */
+
+import server from "./app/server";
+
+server.main();
