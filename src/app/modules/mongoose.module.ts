8491959dc91d602fb00120a4381a39bda5c8cdcf
Roberto Valenzuela <robval.1721@gmail.com>
2020-11-28 10:33:19 -0300

    options added

--- src/app/modules/mongoose.module.ts
+++ src/app/modules/mongoose.module.ts
@@ -2,10 +2,12 @@ import mongoose, { ConnectionOptions } from "mongoose";
 
 function connect(): Promise<typeof mongoose> {
   const options: ConnectionOptions = {
-
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
   }
 
-  return mongoose.connect(`${process.env['MONGO_URI']}/${process.env['MONGO_DATABASE_NAME']}`, options);
+  return mongoose.connect(`${process.env['MONGO_URI']}`, options);
 }
 
 export default { connect };
